4f0409066161041fe0a3d2a3b051fec1052c3068
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-03-18 14:58:04 -0400

    working on render component

---
+++ src/components/common/renderList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Autocomplete from 'react-autocomplete';
+
+const RenderList = ({ dataList, selectedAddress }) => {
+    const items = dataList.map((item, index) => ({
+        id: item._id,
+        label: item.address1,
+    }));
+    return (
+        <div className="p-3 mb-2 bg-secondary text-white">
+            <div className="inner-addon right-addon">
+                <Autocomplete
+                    getItemValue={item => item.label}
+                    items={items}
+                    renderItem={(item, isHighlighted) => (
+                        <div key={item.id} style={{ background: isHighlighted ? 'lightgray' : 'black' }}>
+                            {item.label}
+                        </div>
+                    )}
+                    value={selectedAddress}
+                    onChange={e =>
+                        this.setState({
+                            selectedAddress: e.target.value,
+                            isCollectionScheduleTextHidden: true,
+                            isAutoTextHidden: false,
+                        })
+                    }
+                    onSelect={value =>
+                        this.setState({
+                            selectedAddress: value,
+                            isCollectionScheduleTextHidden: false,
+                            isAutoTextHidden: true,
+                        })
+                    }
+                />
+                <i className="fa fa-search" />
+            </div>
+        </div>
+    );
+}
+
+export default RenderList;
--- src/components/trashlookup.jsx
+++ src/components/trashlookup.jsx
@@ -6,6 +6,7 @@ import InformationSection from './common/InformationSection';
 import { getTrashService } from '../services/trashService';
 import * as moment from 'moment';
 import TrashSchedule from './common/trashSchedule';
+import RenderList from './common/renderList';
 class TrashLookUp extends Form {
   displayCollectionSechduleText() {
     const selectedAddress = _.trim(this.state.selectedAddress);
@@ -84,18 +85,23 @@ class TrashLookUp extends Form {
     const { data } = this.addressData();
     const isCollectionScheduleTextHidden = this.state.isCollectionScheduleTextHidden;
     const isAutoTextHidden = this.state.isAutoTextHidden;
+    const selectedAddress = this.state.selectedAddress;
+    const scheduleType = this.state.scheduleType;
+    const renderWeekOfDay = this.renderWeekOfDay
     return (
       <React.Fragment>
         <h6>Find Your Collection Schedule.</h6>
         <div className="row">
-          <div className="col-5">{!isAutoTextHidden && this.renderList(data)}</div>
+          <div className="col-5">{!isAutoTextHidden && <RenderList dataList={data} selectedAddress={selectedAddress} />}</div>
+          {/* <div className="col-5">{!isAutoTextHidden && this.renderList(data)}</div> */}
+
         </div>
 
         {!isCollectionScheduleTextHidden && this.displayCollectionSechduleText()}
 
         {!isCollectionScheduleTextHidden && <TrashSchedule services={getTrashService()}
-          scheduleType={this.state.scheduleType}
-          renderWeekOfDay={this.renderWeekOfDay} />}
+          scheduleType={scheduleType}
+          renderWeekOfDay={renderWeekOfDay} />}
 
       </React.Fragment>
     );
