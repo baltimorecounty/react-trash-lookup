14fa4fc4dc9e149ab9e6d1516065c8cab4340cc0
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-03-21 12:27:19 -0400

    added code to handle address2

--- src/components/Services/AddressService.jsx
+++ src/components/Services/AddressService.jsx
@@ -1,5 +1,8 @@
 const getFullAddress = address => {
-    return address.length > 0 ? `${address[0].address1}${`, `} ${address[0].city}${`, `} ${address[0].state}${` `} ${address[0].postalCode}` : 'none';
+    return address.length > 0 ? address[0].address2.length > 0
+        ? `${address[0].address1}${`, `}${address[0].address2}${`, `} ${address[0].city}${`, `} ${address[0].state}${` `} ${address[0].postalCode}`
+        : `${address[0].address1}${`, `} ${address[0].city}${`, `} ${address[0].state}${` `} ${address[0].postalCode}`
+        : 'none';
 };
 
 export {
