81414d67945b3a28dd34f48d3a8770414cba1322
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-03-21 13:39:07 -0400

    modified test script to handle address2

--- src/components/Services/AddressService.jsx
+++ src/components/Services/AddressService.jsx
@@ -1,5 +1,5 @@
 const getFullAddress = address => {
-    return address.length > 0 ? address[0].address2.length > 0
+    return address.length > 0 ? address[0].address2.length >0
         ? `${address[0].address1}${`, `}${address[0].address2}${`, `} ${address[0].city}${`, `} ${address[0].state}${` `} ${address[0].postalCode}`
         : `${address[0].address1}${`, `} ${address[0].city}${`, `} ${address[0].state}${` `} ${address[0].postalCode}`
         : 'none';
--- src/components/Services/AddressService.test.js
+++ src/components/Services/AddressService.test.js
@@ -1,8 +1,6 @@
 import { getFullAddress } from './AddressService'
 
 
-const data = [{ address1: "1745 T Street Southeast1", city: "Louisville", state: "KY", postalCode: "40219" }]
-
 describe('parseAddress', () => {
     it('should return a friendly message when the address is empty', () => {
         //arrange 
@@ -10,23 +8,23 @@ describe('parseAddress', () => {
         //act 
         const actual = getFullAddress(emptyAddress);
         //assert
-        expect(actual).toEqual('Address was null or empty');
+        expect(actual).toEqual('1745 T Street Southeast1,  Louisville,  KY  40219');
     })
     it('should return a friendly message when the address is matched', () => {
         //arrange 
-        const emptyAddress = [{ address1: "1745 T Street Southeast1", city: "Louisville", state: "KY", postalCode: "40219" }]
+        const emptyAddress = [{ address1: "1745 T Street Southeast1", address2: "Apt 15", city: "Louisville", state: "KY", postalCode: "40219" }]
         //act 
         const actual = getFullAddress(emptyAddress);
         //assert
-        expect(actual).toEqual('1745 T Street Southeast1,  Louisville,  KY  40219');
+        expect(actual).toEqual('1745 T Street Southeast1, Apt 15,  Louisville,  KY  40219');
     })
 
-    it('should return a friendly message when address2 is not handled by function', () => {
+    it('should return a friendly message when address does not match', () => {
         //arrange 
-        const emptyAddress = [{ address1: "1745 T Street Southeast1", address2: "8958 new street", city: "Louisville", state: "KY", postalCode: "40219" }]
+        const emptyAddress = [{ address1: "1745 T Street Southeast1", address2: "Apt 15", city: "Louisville", state: "KY", postalCode: "40219" }]
         //act 
         const actual = getFullAddress(emptyAddress);
         //assert
-        expect(actual).toEqual('Address2 needs to be handle by function');
+        expect(actual).toEqual('1745 T Street Southeast1, Louisville, KY 40219');
     })
 });
