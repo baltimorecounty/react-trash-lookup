d0cd4b879cd42824f260471eb33a679197fbfeb7
martypowell <jpowel25@gmail.com>
2019-03-21 14:35:35 -0400

    reformat

--- src/services/AddressService.test.js
+++ src/services/AddressService.test.js
@@ -1,30 +1,57 @@
-import { getFullAddress } from './AddressService'
-
+import { getFullAddress } from './AddressService';
 
 describe('parseAddress', () => {
     it('should return a friendly message when the address is empty', () => {
-        //arrange 
+        //arrange
         const emptyAddress = {};
-        //act 
+
+        //act
         const actual = getFullAddress(emptyAddress);
+
         //assert
-        expect(actual).toEqual('1745 T Street Southeast1,  Louisville,  KY  40219');
-    })
+        expect(actual).toEqual(
+            '1745 T Street Southeast1,  Louisville,  KY  40219'
+        );
+    });
     it('should return a friendly message when the address is matched', () => {
-        //arrange 
-        const emptyAddress = [{ address1: "1745 T Street Southeast1", address2: "Apt 15", city: "Louisville", state: "KY", postalCode: "40219" }]
-        //act 
+        //arrange
+        const emptyAddress = [
+            {
+                address1: '1745 T Street Southeast1',
+                address2: 'Apt 15',
+                city: 'Louisville',
+                state: 'KY',
+                postalCode: '40219'
+            }
+        ];
+
+        //act
         const actual = getFullAddress(emptyAddress);
+
         //assert
-        expect(actual).toEqual('1745 T Street Southeast1, Apt 15,  Louisville,  KY  40219');
-    })
+        expect(actual).toEqual(
+            '1745 T Street Southeast1, Apt 15,  Louisville,  KY  40219'
+        );
+    });
 
     it('should return a friendly message when address does not match', () => {
-        //arrange 
-        const emptyAddress = [{ address1: "1745 T Street Southeast1", address2: "Apt 15", city: "Louisville", state: "KY", postalCode: "40219" }]
-        //act 
+        //arrange
+        const emptyAddress = [
+            {
+                address1: '1745 T Street Southeast1',
+                address2: 'Apt 15',
+                city: 'Louisville',
+                state: 'KY',
+                postalCode: '40219'
+            }
+        ];
+
+        //act
         const actual = getFullAddress(emptyAddress);
+
         //assert
-        expect(actual).toEqual('1745 T Street Southeast1, Louisville, KY 40219');
-    })
+        expect(actual).toEqual(
+            '1745 T Street Southeast1, Louisville, KY 40219'
+        );
+    });
 });
