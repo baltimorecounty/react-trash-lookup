a69f6167e4f5a95f1d40fe4622a1e9235318ad40
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-03-22 12:38:43 -0400

    removed one comment

--- src/components/trashlookup.jsx
+++ src/components/trashlookup.jsx
@@ -106,8 +106,6 @@ class TrashLookUp extends Component {
     const selectedAddress = _.trim(this.state.selectedAddress);
     const { data } = this.addressData(isAutoTextHidden);
     const Address = data.length > 0 ? _.assign({ 'address1': data[0].address1, 'address2': data[0].address2, 'city': data[0].city, 'state': data[0].state, 'postalCode': data[0].postalCode }) : {};
-    // const Address = data.length > 0 ? ({ 'address1': data[0].address1, 'address2': data[0].address2, 'city': data[0].city, 'state': data[0].state, 'postalCode': data[0].postalCode }) : {};
-
     const fullAddress = getFullAddress(Address);
     const renderDayofWeek = this.renderDayofWeek
 
