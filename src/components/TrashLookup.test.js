7239729b5dc40e187e6dc3717fe39561ec5793b3
martypowell <jpowel25@gmail.com>
2020-01-07 09:25:36 -0500

    added button click for submitting an address

--- src/components/TrashLookup.test.js
+++ src/components/TrashLookup.test.js
@@ -4,14 +4,16 @@ import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 test("should be able to get address results with a valid address that has data", async () => {
-  const { getByLabelText } = render(<TrashLookUp />);
+  const { getByText, getByLabelText } = render(<TrashLookUp />);
 
+  // Verify form elements
   const addressInput = getByLabelText(/find your collection schedule/i);
+  const submitButton = getByText(/get schedule/i);
 
   await userEvent.type(addressInput, "1745 T Street Southeast1");
 
   // TODO: Unless the user selects an item from the list they cannot see results, this should be able to happen through a button click
   // In the provided mockup the button is the magnifying glass, so this test might slightly need to change
-  //   const submitButton = getByText(/get schedule/i);
-  //   userEvent.click(submitButton);
+
+  userEvent.click(submitButton);
 });
