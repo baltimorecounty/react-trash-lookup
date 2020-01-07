a7e5d37426632afbd93615e9af2f3038b20086ec
martypowell <jpowel25@gmail.com>
2020-01-07 09:06:32 -0500

    updated test wtih todo

--- src/components/TrashLookup.test.js
+++ src/components/TrashLookup.test.js
@@ -1,10 +1,17 @@
-import { fireEvent, render } from "@testing-library/react";
-
 import React from "react";
 import TrashLookUp from "./trashlookup";
+import { render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test("should be able to get address results", () => {
+test("should be able to get address results with a valid address that has data", async () => {
   const { getByLabelText } = render(<TrashLookUp />);
 
   const addressInput = getByLabelText(/find your collection schedule/i);
+
+  await userEvent.type(addressInput, "1745 T Street Southeast1");
+
+  // TODO: Unless the user selects an item from the list they cannot see results, this should be able to happen through a button click
+  // In the provided mockup the button is the magnifying glass, so this test might slightly need to change
+  //   const submitButton = getByText(/get schedule/i);
+  //   userEvent.click(submitButton);
 });
